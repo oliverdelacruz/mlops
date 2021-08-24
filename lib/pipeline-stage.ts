62d0883ee9d1a1adf19926029708c0aba4c13224
Oliver De La Cruz <cruzoliv@amazon.com>
2021-08-24 15:34:29 +0200

    MESSAGE

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,10 +1,14 @@
 import { MlOpsStack } from "./ml_ops-stack";
-import { Stage, Construct, StageProps } from "@aws-cdk/core";
+import { Stage, CfnOutput, Construct, StageProps } from "@aws-cdk/core";
 
 export class WorkshopPipelineStage extends Stage {
+  public readonly hcViewerUrl: CfnOutput;
+  public readonly hcEndpoint: CfnOutput;
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    new MlOpsStack(this, "WebService");
+    const service = new MlOpsStack(this, "WebService");
+    this.hcEndpoint = service.hcEndpoint;
+    this.hcViewerUrl = service.hcViewerUrl;
   }
 }
