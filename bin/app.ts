9ace97f4bc37c1bd2e16ecf44714552640a21283
Oliver De La Cruz <cruzoliv@amazon.com>
2021-12-05 17:58:51 +0100

    Fixed bug on ecs service stack

--- bin/app.ts
+++ bin/app.ts
@@ -17,4 +17,4 @@ new PipelineStack(app, "CdkPipelineStack", {
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
-new PipelineStage(app, "CdkPipelineStage", {});
+// new PipelineStage(app, "CdkPipelineStage", {});
