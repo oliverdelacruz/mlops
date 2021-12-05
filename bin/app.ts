017fab27b3b3f63061af9ea5b9a14b650ba05640
Oliver De La Cruz <cruzoliv@amazon.com>
2021-12-05 20:36:36 +0100

    Fixed bug on ecs service stack

--- bin/app.ts
+++ bin/app.ts
@@ -6,21 +6,7 @@ import { PipelineStageStack } from "../lib/stage-stack";
 
 const app = new cdk.App();
 
-// const pipelineStack = new PipelineStack(app, "CdkPipelineStack", {
-//   env: { account: "754427201920", region: "eu-west-1" },
-//   /* If you don't specify 'env', this stack will be environment-agnostic.
-//    * Account/Region-dependent features and context lookups will not work,
-//    * but a single synthesized template can be deployed anywhere. */
-//   /* Uncomment the next line to specialize this stack for the AWS Account
-//    * and Region that are implied by the current CLI configuration. */
-//   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-//   /* Uncomment the next line if you know exactly what Account and Region you
-//    * want to deploy the stack to. */
-//   // env: { account: '123456789012', region: 'us-east-1' },
-//   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-// });
-
-const pipelineStageStack = new PipelineStageStack(app, "CdkPipelineStageStack", {
+const pipelineStack = new PipelineStack(app, "PipelineStack", {
   env: { account: "754427201920", region: "eu-west-1" },
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -33,3 +19,17 @@ const pipelineStageStack = new PipelineStageStack(app, "CdkPipelineStageStack", 
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+// const pipelineStageStack = new PipelineStageStack(app, "CdkPipelineStageStack", {
+//   env: { account: "754427201920", region: "eu-west-1" },
+//   /* If you don't specify 'env', this stack will be environment-agnostic.
+//    * Account/Region-dependent features and context lookups will not work,
+//    * but a single synthesized template can be deployed anywhere. */
+//   /* Uncomment the next line to specialize this stack for the AWS Account
+//    * and Region that are implied by the current CLI configuration. */
+//   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+//   /* Uncomment the next line if you know exactly what Account and Region you
+//    * want to deploy the stack to. */
+//   // env: { account: '123456789012', region: 'us-east-1' },
+//   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// });
